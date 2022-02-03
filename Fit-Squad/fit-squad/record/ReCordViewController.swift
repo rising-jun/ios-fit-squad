6dad547789f8df871ffc53837c647049c7a40ff4
BumgeunSong <isp1195@gmail.com>
2022-02-03 17:04:01 +0900

    Add Tableview

--- Fit-Squad/fit-squad/record/ReCordViewController.swift
+++ Fit-Squad/fit-squad/record/ReCordViewController.swift
@@ -17,6 +17,7 @@ class RecordViewController: BaseViewController {
 }
 
 
+
 #if DEBUG
 import SwiftUI
 
--- Fit-Squad/fit-squad/record/RecordView.swift
+++ Fit-Squad/fit-squad/record/RecordView.swift
@@ -14,24 +14,37 @@ class RecordView: BaseView {
     lazy var startButton: UIButton = {
         var button = UIButton()
         button.setTitle("운동 추가", for: .normal)
+        button.backgroundColor = .systemBlue
         button.layer.cornerRadius = 5
         return button
     }()
     
+    lazy var routineTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.backgroundColor = .opaqueSeparator
+        return tableView
+    }()
     
     override func setupUI() {
         super.setupUI()
         backgroundColor = .white
         
         addSubview(startButton)
-        
-        startButton.backgroundColor = .systemBlue
-        
+
         startButton.snp.makeConstraints { make in
             make.height.equalTo(50)
             make.bottom.equalTo(self.safeAreaLayoutGuide).inset(100)
             make.left.equalTo(self.safeAreaLayoutGuide).inset(30)
             make.right.equalTo(self.safeAreaLayoutGuide).inset(30)
+        }
+        
+        addSubview(routineTableView)
+        
+        routineTableView.snp.makeConstraints { make in
+            make.top.equalTo(self.safeAreaLayoutGuide).inset(10)
+            make.bottom.equalTo(startButton).inset(30)
+            make.leading.equalTo(self.safeAreaLayoutGuide).inset(10)
+            make.trailing.equalTo(self.safeAreaLayoutGuide)
         }
     }
 }
