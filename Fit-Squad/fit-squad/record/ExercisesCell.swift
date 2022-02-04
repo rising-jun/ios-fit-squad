b7fb26a0fc4d96f998c6640991c58f1b0fa56745
BumgeunSong <isp1195@gmail.com>
2022-02-04 15:18:07 +0900

    Create SetupUI

--- Fit-Squad/fit-squad/record/ExercisesCell.swift
+++ Fit-Squad/fit-squad/record/ExercisesCell.swift
@@ -14,10 +14,16 @@ class ExercisesCell: UITableViewCell {
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupUI()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupUI() {
+        // 운동 이름, 횟수, 세트, 중량의 뷰를 설정
+        
     }
     
     override func awakeFromNib() {
